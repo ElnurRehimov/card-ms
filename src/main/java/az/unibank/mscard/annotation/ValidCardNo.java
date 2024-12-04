f9f13df9a99e099a0f7dfda3cda2c04a52c8a28d
Elnur <elnur.rahimov@expressbank.az>
2024-12-05 01:43:49 +0400

    Added card validator

--- src/main/java/az/unibank/mscard/annotation/ValidCardNo.java
+++ src/main/java/az/unibank/mscard/annotation/ValidCardNo.java
@@ -13,23 +13,10 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidCardNo {
 
-    /**
-     * Default error message if the items is not valid.
-     *
-     * @return : error message.
-     */
     String message() default "The list items is not valid";
 
-    /**
-     * @return the groups the constraint belongs to
-     */
     Class<?>[] groups() default {};
 
-    /**
-     * The payload of the parameter.
-     *
-     * @return the payload associated to the constraint
-     */
     Class<? extends Payload>[] payload() default {};
 
 }
